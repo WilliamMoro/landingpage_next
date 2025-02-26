329765cce33d6634bca02f76dece3fb241c40aed
WilliamMoro <wmoro@ekan.com.br>
2025-02-26 00:16:50 -0300

    Implementado responsividade no scss de todos os componentes, alterado imagem banner e index.jsx

--- components/welcome/index.jsx
+++ components/welcome/index.jsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
 import Button from "../button";
 import StyleWelcome from "./welcome.module.scss";
-import BannerWelcome from "../../public/assets/image/slogan.png";
+import BannerWelcome from "../../public/assets/image/slogan.svg";
 
 const Welcome = () => {
-    return <div className={StyleWelcome.container}>
-        <div className={StyleWelcome.text}>
-            <h1>Melhor Agência de marketing da cidade!</h1>
-            <p>
-                Somos uma agência de performance digital, aceleramos vendas e aquisições de leads para grander marcas.
-            </p>
-            <Button title="Aumentar Vendas" kind="secundary"></Button>
+    return (
+        <div className={StyleWelcome.container}>
+            <div className={StyleWelcome.text}>
+                <h1>Melhor Agência de marketing da cidade!</h1>
+                <p>
+                    Somos uma agência de performance digital, aceleramos vendas e aquisições de leads para grander marcas.
+                </p>
+                <Button title="Aumentar Vendas" kind="secundary"></Button>
+            </div>
+            <div className={StyleWelcome.image}>
+                <Image className={StyleWelcome.banner} src={BannerWelcome} alt="Banner Welcome"></Image>
+            </div>
         </div>
-        <div className={StyleWelcome.image}>
-            <Image className={StyleWelcome.banner} src={BannerWelcome} alt="Banner Welcome"></Image>
-        </div>
-    </div>
-}
+    );
+};
 
 export default Welcome;
